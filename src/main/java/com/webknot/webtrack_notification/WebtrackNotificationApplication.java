3b35b789ff1a47969e3c9a42ebc4fd8b6de4a807
anuragWKT <anurag.s@webknot.in>
2026-01-31 23:56:20 +0530

    initial project sturcture

---
+++ src/main/java/com/webknot/webtrack_notification/WebtrackNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.webknot.webtrack_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtrackNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtrackNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webknot/webtrack_notification/WebtrackNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webknot.webtrack_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtrackNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
